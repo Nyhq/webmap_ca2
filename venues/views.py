bad48d9debce7ac647f43e5f4a2470177ef58d59
Seán Breen <seanbreen98@gmail.com>
2023-12-30 13:24:39 +0000

    updated reqs.txt 2

--- venues/views.py
+++ venues/views.py
@@ -2,9 +2,6 @@ from rest_framework import viewsets
 from .models import Venue, Review
 from .serializers import VenueSerializer, ReviewSerializer
 from django.contrib.auth.models import User
-from django.http import JsonResponse
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
